46cfa71ed22cf5d490b31b222679c9c54c73d0cc
Omar I. Martínez Gómez <omar_238@hotmail.com>
2018-10-08 01:11:45 -0500

    .

--- librosweb.es/cap04/scripts/script.js
+++ librosweb.es/cap04/scripts/script.js
@@ -1,7 +1,8 @@
 window.onload = function () {
 	var nuevoP = document.createElement('p');
-	var texto = document.createTextNode('Segundo párrafo');
+	var texto = document.createTextNode('Segundo párrafo, antes del primero');
 	nuevoP.appendChild(texto);
 
-	document.body.appendChild(nuevoP);
+	var anteriorP = document.getElementsByTagName('p')[0];
+	document.body.insertBefore(nuevoP, anteriorP);
 }
